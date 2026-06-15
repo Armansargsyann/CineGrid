db306db7a8125d50463ca0c4d674f827935b3a4d
Armansargsyann <armansargsyan1249@gmail.com>
2026-06-15 17:45:14 +0400

    feat(table): configure DataGrid columns with custom rendering

---
+++ src/components/MoviesTable.tsx
@@ -0,0 +1,34 @@
+import type { GridColDef } from '@mui/x-data-grid';
+import { RatingTypography, TitleTypography, PosterImage } from './styled/MovieStyles';
+
+export const columns: GridColDef[] = [
+  { 
+    field: 'poster_path', 
+    headerName: 'Poster', 
+    width: 100,
+    renderCell: (params) => (  //by this we are control oure recourse
+      <PosterImage 
+      src={`${import.meta.env.VITE_TMDB_IMAGE_BASE_URL}${params.value}`}
+        alt="poster" 
+      />
+    )
+  },
+  { 
+    field: 'title', 
+    headerName: 'Title', 
+    flex: 1,
+    renderCell: (params) => <TitleTypography>{params.value}</TitleTypography>
+  },
+  { field: 'release_date', headerName: 'Release Date', width: 150 },
+  { 
+    field: 'vote_average', 
+    headerName: 'Rating', 
+    type: 'number',
+    width: 120,
+    renderCell: (params) => (
+      <RatingTypography rating={params.value as number}>
+        {params.value}
+      </RatingTypography>
+    )
+  }
+];
